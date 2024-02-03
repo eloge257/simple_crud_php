0d00999628da2ffbea5f573ab2e0e2a16731bb1b
eloge257 <niremaeloge@gmail.com>
2024-02-03 22:39:03 +0200

    connect our app with database

---
+++ database/database.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * author eloge257
+ * Dans cette fichier nous mettons les codes qui nous permet de connecte notre application avec la base de donnees
+ * Date le 03/02/2024
+ */
+
+
+
+?>
