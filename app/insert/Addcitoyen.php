5a1176779a3b63d248ceebae9307a9465d22579b
eloge257 <niremaeloge@gmail.com>
2024-02-05 10:57:50 +0200

    add form

--- app/insert/Addcitoyen.php
+++ app/insert/Addcitoyen.php
@@ -3,10 +3,45 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="shortcut icon" href="../../public/profile.png" type="image/png">
+    <title>Simple CRUD with PHP</title>
 </head>
 <body>
-    
+
+    <div class="card">
+        <div class="card-header">
+            <h1>Add citoyen</h1>
+        </div>
+        <div class="card-contenu">
+            <form action="" method="POST">
+              <div>
+                  <label for="">Nom</label>
+                 <input type="text" name="NOM" id="">
+              </div>
+              <div>
+                  <label for="">Prenom</label>
+                 <input type="text" name="PRENOM" id="">
+              </div>
+              <div>
+                  <label for="">Adresse</label>
+                 <input type="text" name="ADRESSE" id="">
+              </div>
+              <div>
+                  <label for="">Genre</label>
+                  <select name="" id="" disabled="disabled">
+                      <option>--genre--</option>
+                      <option value="1">Masculin</option>
+                      <option value="2">Feminim</option>
+                  </select>
+              </div>
+              <div>
+                  <label for="">Nationalite</label>
+                 <input type="text" name="NOM" id="">
+              </div>
+            </form>
+        </div>
+    </div>
+
     
 </body>
 </html>
